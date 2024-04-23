4b6afb523b0c8a0d655ec2ce2aa5e9954813bb69
sinminseok12 <sinminseok12@naver.com>
2024-04-23 17:19:04 +0900

    feat : Job 도메인 설계

---
+++ fl-core/src/main/java/com/core/job/repotiory/querydsl/CustomJobRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.core.job.repotiory.querydsl;
+
+
+import com.core.job.model.Job;
+
+import java.util.List;
+
+public class CustomJobRepositoryImpl implements CustomJobRepository {
+    @Override
+    public List<Job> findByFilter() {
+        return null;
+    }
+}
