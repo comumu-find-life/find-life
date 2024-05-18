1f299aae8f4a1aca7a5345b1636369812c1867de
master <onagu7167@gmal.com>
2024-05-18 09:26:58 +0900

    chat

--- fl-batch/src/main/java/com/batch/login/LoginController.java
+++ fl-batch/src/main/java/com/batch/login/LoginController.java
@@ -1,11 +1,18 @@
 package com.batch.login;
 
 import com.service.home.dto.SimpleHomeDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -14,16 +21,36 @@ public class LoginController {
     @Value("${domain.api}")
     private String baseUrl;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/login")
     public String loginPage() {
         return "login/login";
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody LoginFormDto loginFormDto) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = baseUrl + "/login";
+    public String login(LoginFormDto loginFormDto, HttpServletResponse response) {
+        System.out.println("login-start");
+        String url ="http://localhost:8080/login";
 
-        SimpleHomeDto[] homeDtos = restTemplate.getForObject(url, SimpleHomeDto[].class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<LoginFormDto> requestEntity = new HttpEntity<>(loginFormDto, headers);
+
+        try {
+            JwtTokenDto tokenDto = restTemplate.postForObject(url, requestEntity, JwtTokenDto.class);
+
+            Cookie cookie = new Cookie("Authorization", tokenDto.getAccessToken());
+            cookie.setPath("/");
+            cookie.setHttpOnly(true); // 서버만 쿠키에 접근
+            cookie.setSecure(false);
+            response.addCookie(cookie);
+        } catch (Exception e) {
+            return "login/login";
+        }
+
+        return "redirect:/";
     }
 }
