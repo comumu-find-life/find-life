c12cb7a3167a656f59274b4147f6ab92ea07200b
master <onagu7167@gmal.com>
2024-05-29 14:00:36 +0900

    Batch css

--- fl-batch/src/main/java/com/batch/login/LoginCheckInterceptor.java
+++ fl-batch/src/main/java/com/batch/login/LoginCheckInterceptor.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.net.HttpCookie;
 import java.util.Arrays;
@@ -24,7 +25,6 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         String requestURI = request.getRequestURI();
-
         Cookie[] cookies = request.getCookies();
 
         // "Authorization" 쿠키 찾기
@@ -36,49 +36,61 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
             // Authorization 쿠키가 있는지 확인
             if (authCookie == null) {
-                response.sendRedirect("/login?redirectURL=" + requestURI);
-                return false;
-            }
-            String jwtToken = authCookie.getValue();
-            RestTemplate restTemplate = new RestTemplate();
-
-            // HttpHeaders 객체 생성
-            HttpHeaders headers = new HttpHeaders();
-            // Bearer 토큰 추가
-            headers.setBearerAuth(jwtToken);
-            // 요청을 보낼 때 JSON으로 요청하기 위해 Content-Type 설정
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
-            // HttpEntity 객체 생성 (요청 본문 및 헤더 설정)
-            HttpEntity<String> requestEntity = new HttpEntity<>(headers);
-
-            // 요청 보내기
-            ResponseEntity<UserInformationDto> responseEntity = restTemplate.exchange(
-                    "http://localhost:8080/v1/api/user/user-info",
-                    HttpMethod.GET,
-                    requestEntity,
-                    UserInformationDto.class);
-
-            // 응답 확인
-            HttpStatus statusCode = (HttpStatus) responseEntity.getStatusCode();
-
-            System.out.println(statusCode == HttpStatus.OK);
-            if (statusCode == HttpStatus.OK) {
-                UserInformationDto responseBody = responseEntity.getBody();
-                System.out.println("Response Body: " + responseBody);
-
-                request.setAttribute("userId", responseBody.getId());
-                request.setAttribute("accessToken", jwtToken);
-                return true;
+//                response.sendRedirect("/login?redirectURL=" + requestURI);
+//                return false;
             } else {
-                System.err.println("Request failed with status code: " + statusCode);
-                return false;
+                String jwtToken = authCookie.getValue();
+                RestTemplate restTemplate = new RestTemplate();
+
+                // HttpHeaders 객체 생성
+                HttpHeaders headers = new HttpHeaders();
+                // Bearer 토큰 추가
+                headers.setBearerAuth(jwtToken);
+                // 요청을 보낼 때 JSON으로 요청하기 위해 Content-Type 설정
+                headers.setContentType(MediaType.APPLICATION_JSON);
+
+                // HttpEntity 객체 생성 (요청 본문 및 헤더 설정)
+                HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+
+                // 요청 보내기
+                ResponseEntity<UserInformationDto> responseEntity = restTemplate.exchange(
+                        "http://localhost:8080/v1/api/user/user-info",
+                        HttpMethod.GET,
+                        requestEntity,
+                        UserInformationDto.class);
+
+                // 응답 확인
+                HttpStatus statusCode = (HttpStatus) responseEntity.getStatusCode();
+
+                System.out.println(statusCode == HttpStatus.OK);
+                if (statusCode == HttpStatus.OK) {
+                    UserInformationDto responseBody = responseEntity.getBody();
+                    System.out.println("Response Body: " + responseBody);
+
+
+                    request.setAttribute("userId", responseBody.getId());
+                    request.setAttribute("accessToken", jwtToken);
+                    return true;
+                } else {
+                    System.err.println("Request failed with status code: " + statusCode);
+                    return true;
+                }
             }
 
         }
 
         // Authorization 쿠키가 없으면 로그인 페이지로 리디렉션
-        response.sendRedirect("/login?redirectURL=" + requestURI);
-        return false;
+//        response.sendRedirect("/login?redirectURL=" + requestURI);
+//        return false;
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        if (modelAndView != null) {
+            if (request.getAttribute("userId") != null) modelAndView.addObject("isLogged", true);
+            else                                              modelAndView.addObject("isLogged", false);
+        }
     }
 }
