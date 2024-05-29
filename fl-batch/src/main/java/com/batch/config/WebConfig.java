b75261287b7fb693888aefb4872d7cdd13b5c629
master <onagu7167@gmal.com>
2024-05-29 14:00:36 +0900

    Batch css

--- fl-batch/src/main/java/com/batch/config/WebConfig.java
+++ fl-batch/src/main/java/com/batch/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .addPathPatterns("/dms", "/dm/**");
+        registry.addInterceptor(new LoginCheckInterceptor());
+//                .addPathPatterns("/dms", "/dm/**");
     }
 }
