68e4346362087cadfb2a0ff54abd1ba19bb1d676
sinminseok12 <sinminseok12@naver.com>
2024-06-28 04:41:37 +0900

    feat : jwt 로 자신의 userId 조회하는 api 구현

--- fl-api/src/main/java/com/api/user/UserController.java
+++ fl-api/src/main/java/com/api/user/UserController.java
@@ -1,16 +1,22 @@
 package com.api.user;
 
+import com.api.security.service.JwtService;
 import com.service.user.UserService;
 import com.service.user.dto.UserInformationDto;
 import com.service.user.dto.UserProfileResponse;
 import com.service.user.dto.UserSignupRequest;
 import com.service.utils.SuccessResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 
 @RestController
@@ -19,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
     /**
      * 회원가입 api
@@ -42,6 +49,19 @@ public class UserController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * jwt 로 자신의 userId 조회하기.
+     */
+
+    @GetMapping("/me/userId")
+    @PreAuthorize("hasAnyRole('ROLE_GETTER')")
+    public ResponseEntity<?> getMyUserId(HttpServletRequest request) {
+        String accessToken  = jwtService.extractAccessToken(request).get();
+        String email = jwtService.extractEmail(accessToken).get();
+        Long userId = userService.findByEmail(email).getId();
+        SuccessResponse response = new SuccessResponse(true, SuccessUserMessages.MY_USER_ID_RETRIEVE_SUCCESS, userId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     /**
      * 다른 사용자가 프로필을 조회하는 api
      */
--- fl-chatting/src/main/java/com/chatting/controller/DirectMessageController.java
+++ fl-chatting/src/main/java/com/chatting/controller/DirectMessageController.java
@@ -14,7 +14,7 @@ import java.sql.SQLOutput;
 @RequiredArgsConstructor
 public class DirectMessageController {
 
-    private final SimpMessagingTemplate template; //특정 broker로 매제지 전달
+    private final SimpMessagingTemplate template; //특정 broker로 매제지 전달, STOMP 프로토콜을 통해 메시지를 브로커로 전달한다.
     private final DirectMessageService dmService;
 
 
@@ -29,6 +29,9 @@ public class DirectMessageController {
 //        /chat/room/" + message.getRoomId(), message);
     }
 
+    /**
+     * 클라이언트가 메시지를 전송할때 호출되며, 수신된 메시지를 처리하고 브로커를 통해 해당 채팅방에 미시지를 브로드 캐스트 한다.
+     */
     @MessageMapping(value = "/chat/message")
     public void message(DirectMessageDto dmDto){
         System.out.println(dmDto.getMessage());
@@ -39,6 +42,9 @@ public class DirectMessageController {
         template.convertAndSend("/sub/chat/room/" + dmDto.getRoomId(), dmDto);
     }
 
+    /**
+     *
+     */
     @MessageMapping(value = "/dm/message")
     public void directMessage(DirectMessageDto dmDto){
         template.convertAndSend("/sub/chat/room/" + dmDto.getRoomId(), dmDto.getMessage());
