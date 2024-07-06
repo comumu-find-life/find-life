7625fa2d30ac0af39e475471abac0eaace492ee4
sinminseok12 <sinminseok12@naver.com>
2024-07-06 16:46:53 +0900

    feat : 채팅 목록 조회 api 구현

--- fl-chatting/src/main/java/com/chatting/service/DirectMessageService.java
+++ fl-chatting/src/main/java/com/chatting/service/DirectMessageService.java
@@ -18,7 +18,6 @@ public class DirectMessageService {
     private final DirectMessageRepository dmRepository;
 
     public String sendDM(DirectMessageDto dmDto) {
-
         try {
             DirectMessage save = dmRepository.save(
                 DirectMessage.builder()
@@ -43,6 +42,19 @@ public class DirectMessageService {
 
         List<DirectMessageResponse> dmLogDtos = dmLogs.stream()
                 .map(dm -> DirectMessageResponse.builder()
+                        .senderId(dm.getSenderId())
+                        .receiverId(dm.getReceiverId())
+                        .message(dm.getMessage())
+                        .sentAt(dm.getSentAt())
+                        .build())
+                .collect(Collectors.toList());
+        return dmLogDtos;
+    }
+
+    public List<DirectMessageDto> findChatHistory(Long user1Id, Long user2Id) {
+        List<DirectMessage> dmLogs = dmRepository.findRecentLogs(user1Id, user2Id);
+        List<DirectMessageDto> dmLogDtos = dmLogs.stream()
+                .map(dm -> DirectMessageDto.builder()
                         .senderId(dm.getSenderId())
                         .receiverId(dm.getReceiverId())
                         .message(dm.getMessage())
