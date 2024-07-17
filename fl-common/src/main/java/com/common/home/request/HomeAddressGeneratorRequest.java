d1a7f88fece6fd0ffb973f2f9502dc7e324424bf
master <onagu7167@gmal.com>
2024-07-17 16:37:35 +0900

    batch refactor

--- fl-common/src/main/java/com/common/home/request/HomeAddressGeneratorRequest.java
+++ fl-common/src/main/java/com/common/home/request/HomeAddressGeneratorRequest.java
@@ -7,20 +7,20 @@ import lombok.Getter;
 @Builder
 public class HomeAddressGeneratorRequest {
 
+    // ex) 3000 우편번호
+    private Integer postCode;
+
     // ex) NSW
     private String state;
 
     // ex) Sydney
     private String city;
 
-    // ex) 3000 우편번호
-    private Integer postCode;
+    // ex) Bridge Street 거리 이름
+    private String streetName;
 
     // ex) 상세 주소 ex) 401 호
     private String detailAddress;
-
-    // ex) Bridge Street 거리 이름
-    private String streetName;
 
     // ex) 건물 번호 10
     private String streetCode;
