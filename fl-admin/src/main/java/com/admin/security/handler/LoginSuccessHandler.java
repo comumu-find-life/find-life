284b866296cb60a1da21b19bc263805935af7282
sinminseok12 <sinminseok12@naver.com>
2024-07-24 05:00:39 +0900

    feat : 모든 회웑 정보 조회 API 구현

---
+++ fl-admin/src/main/java/com/admin/security/handler/LoginSuccessHandler.java
@@ -0,0 +1,57 @@
+package com.admin.security.handler;
+import com.admin.security.service.JwtService;
+import com.redis.user.UserRedisService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRedisService userRedisService;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        String email = extractUsername(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String accessToken = jwtService.createAccessToken(email); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
+        String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
+
+        userRedisService.saveRefreshToken(email, refreshToken);
+        userRedisService.findUserByEmail(email);
+
+        printSuccessMessage(email, accessToken);
+    }
+
+    private void printSuccessMessage(String email, String accessToken){
+        log.info("로그인에 성공하였습니다. 이메일 : {}", email);
+        log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
+        log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
+    }
+
+
+    private String extractUsername(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+
+        System.out.println(userDetails.getUsername());
+        return userDetails.getUsername();
+    }
+}
