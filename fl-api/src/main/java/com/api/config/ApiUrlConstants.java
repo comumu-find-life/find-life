cca043a44fe9639d9ec239b407994c65dd9c4c8a
sinminseok12 <sinminseok12@naver.com>
2024-09-11 19:04:31 +0900

    feat : 이메일 인증 번호 redis 구현

--- fl-api/src/main/java/com/api/config/ApiUrlConstants.java
+++ fl-api/src/main/java/com/api/config/ApiUrlConstants.java
@@ -24,6 +24,11 @@ public class ApiUrlConstants {
     // 사용자 API URL
     public static final String USERS_BASE_URL = BASE_API_URL + "/users";
     public static final String USERS_SIGN_UP = USERS_BASE_URL + "/sign-up";
+    public static final String USERS_CHECK_DUPLICATE_EMAIL = USERS_BASE_URL + "/verification/email/{email}";
+    public static final String USERS_SEND_VERIFICATION_EMAIL = USERS_BASE_URL+"/verification/email/send/{email}";
+    public static final String USERS_CONFIRM_VERIFICATION_EMAIL = USERS_BASE_URL+"/verification/email/confirm/{code}";
+
+
     public static final String USERS_FIND_BY_ID = USERS_BASE_URL + "/{userId}";
     public static final String USERS_UPDATE = USERS_BASE_URL + "/{userId}";
     public static final String USERS_GET_MY_USER_ID = USERS_BASE_URL + "/me/userId";
---
+++ fl-redis/src/main/java/com/redis/user/entity/VerificationCode.java
@@ -0,0 +1,27 @@
+package com.redis.user.entity;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+
+/**
+ * RedisHash 는 해당 클래스가 Redis 에 해시 형태로 저장될 엔티티임을 나타냄
+ */
+@Getter
+@Setter
+@RedisHash(value = "VerificationCode", timeToLive = 300) // TTL: 5분
+public class VerificationCode implements Serializable {
+
+    @Id
+    private String email;
+    private String code;
+
+    public VerificationCode(String email, String code) {
+        this.email = email;
+        this.code = code;
+    }
+}
