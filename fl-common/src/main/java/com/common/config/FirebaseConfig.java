6fa72a8858d1322e7f371a3992cea5b4759d6995
sinminseok12 <sinminseok12@naver.com>
2025-01-11 03:53:53 +0900

    fixup! feat : 관리자 페이지 제작

--- fl-common/src/main/java/com/common/config/FirebaseConfig.java
+++ fl-common/src/main/java/com/common/config/FirebaseConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+
 import java.io.*;
 @Configuration
 public class FirebaseConfig {
